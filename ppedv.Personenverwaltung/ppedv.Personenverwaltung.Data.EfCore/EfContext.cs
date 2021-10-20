938ee5238c897dd57b3bf9b8a2845b69fd2fc95d
André <AndreR@outlook.de>
2021-10-20 11:27:20 +0200

    EfCore config übernommen

---
+++ ppedv.Personenverwaltung/ppedv.Personenverwaltung.Data.EfCore/EfContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.Personenverwaltung.Model;
+
+namespace ppedv.Personenverwaltung.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Abteilung> Abteilungen { get; set; }
+        public DbSet<Mitarbeiter> Mitarbeiter { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+
+        public EfContext()
+        {
+            //Database.EnsureCreated();
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Personenverwaltung_Dev;Trusted_Connection=true");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TpT
+            modelBuilder.Entity<Mitarbeiter>().ToTable("Mitarbeiter");
+            //modelBuilder.Entity<Kunde>().ToTable("Customer");
+            modelBuilder.Entity<Kunde>().ToTable("Kunde");
+            modelBuilder.Entity<Person>().ToTable("Person");
+
+            modelBuilder.Entity<Mitarbeiter>().Property(x => x.Beruf).HasMaxLength(48).IsRequired();
+        }
+
+    }
+}
